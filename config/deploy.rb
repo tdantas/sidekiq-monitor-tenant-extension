6c2b521056fd39bd3efd78e39b8548a1f7d6213c
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-05-19 22:16:00 +0100

    identation plus remove usused task

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,30 +25,23 @@ set :use_sudo, false
 set :port, 22
 
 namespace :deploy do
-    # Restart passenger on deploy
-    desc "Restarting mod_rails with restart.txt"
-    task :restart, :roles => :app, :except => { :no_release => true } do
-        run "touch #{release_path}/tmp/restart.txt"
-    end
+  # Restart passenger on deploy
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+      run "touch #{release_path}/tmp/restart.txt"
+  end
 
-    [:start, :stop].each do |t|
-        desc "#{t} task is a no-op with mod_rails"
-        task t, :roles => :app do ; end
-    end
+  [:start, :stop].each do |t|
+      desc "#{t} task is a no-op with mod_rails"
+      task t, :roles => :app do ; end
+  end
 
-    desc "copies configuration files"
-    task :copy_configuration_files, :roles => :app do
-        run "mkdir -p #{shared_path}/config"
-        run_locally "scp app/config/config.yml #{runner}@#{domain}:#{shared_path}/config/config.yml"
-        run "ln -sf #{shared_path}/config/config.yml #{release_path}/app/config/config.yml"
-    end
-end
-
-namespace :passenger do
-    desc "Restart Application"
-    task :restart do
-        run "sudo /etc/init.d/apache2 restart"
-    end
+  desc "copies configuration files"
+  task :copy_configuration_files, :roles => :app do
+      run "mkdir -p #{shared_path}/config"
+      run_locally "scp app/config/config.yml #{runner}@#{domain}:#{shared_path}/config/config.yml"
+      run "ln -sf #{shared_path}/config/config.yml #{release_path}/app/config/config.yml"
+  end
 end
 
 before "deploy:finalize_update", "deploy:copy_configuration_files"
