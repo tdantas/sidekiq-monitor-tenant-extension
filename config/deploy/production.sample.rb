81529fbe2613ac976d02798f86e94da7b8a24178
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-05-19 22:14:12 +0100

    deploy environment samples

---
+++ config/deploy/production.sample.rb
@@ -0,0 +1,17 @@
+puts "\e[0;35m =======================================================\e[0m\n"
+puts "\e[0;35m              DEPLOYING TO PRODUCTION\e[0m\n"
+puts "\e[0;35m =======================================================\e[0m\n"
+
+set :rails_env, "production"
+set :domain, '<HOST>'
+set :runner, "<USER>"
+
+server domain, :app, :web
+
+role :db, domain, :primary => true
+
+ssh_options[:forward_agent] = true
+ssh_options[:user] = runner
+
+set :branch, "master"
+set :deploy_to, "/var/www/#{application}"
